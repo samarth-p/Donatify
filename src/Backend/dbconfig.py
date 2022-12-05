c0b711ef9d58adbfeb31198f637c479982a498c5
samarth-p <42717178+samarth-p@users.noreply.github.com>
2022-12-05 13:14:53 -0500

    Update dbconfig.py

--- src/Backend/dbconfig.py
+++ src/Backend/dbconfig.py
@@ -2,9 +2,9 @@ from asyncio import constants
 
 # Constant Variables
 constants = {
-    "host": "localhost",
+    "host": "127.0.0.1",
     "user": "root",
     "password": "password",
     "database": "donationsystem",
-    "port": "8888"
+    "port": 8888
 }
